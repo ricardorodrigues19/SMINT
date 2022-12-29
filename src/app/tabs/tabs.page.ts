77b72d83c4cc0a6eaaf062a733c6cc26301c0652
Ricardo Rodrigues <ricardo.rodrigues1911@gmail.com>
2022-12-29 03:02:24 +0000

    adding dynamic dark/light mode toggle

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
-
+  
+  darkMode: boolean = false;
   constructor() {}
-
+  
+  toggleTheme(ev: any): void {
+    document.body.classList.toggle('dark', ev.detail.checked);
+  }
 }
