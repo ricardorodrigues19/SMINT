0b8df40a6967e63028d6b863101efa258d7bc57d
Ricardo Rodrigues <ricardo.rodrigues1911@gmail.com>
2023-01-08 16:50:34 +0000

    app fully working well, i guess, api requests done

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +7,12 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+
+  idiomas: any []= [];
+  software: any []= [];
+  lingprog: any []= [];
+  educacao: any []= [];
+  projects: any []= [];
 
   isModalIdiomasOpen = false;
   isModalSoftwareOpen = false;
@@ -46,5 +52,45 @@ export class Tab2Page {
 
   toggleTheme(ev: any): void {
     document.body.classList.toggle('dark', ev.detail.checked);
+  }
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    console.log('Api working');
+    this.http.get('http://localhost:4242/api/idiomas').subscribe(res => {
+      this.idiomas = (res as any).Idiomas;
+    }, error => {
+      console.error(error);
+    });
+  
+    this.http.get('http://localhost:4242/api/software').subscribe(res => {
+      this.software = (res as any).Software;
+    }, error => {
+      console.error(error);
+    });
+
+    this.http.get('http://localhost:4242/api/lingprog').subscribe(res => {
+      this.lingprog = (res as any).LingProg;
+    }, error => {
+      console.error(error);
+    });
+
+    this.http.get('http://localhost:4242/api/educacao').subscribe(res => {
+      this.educacao = (res as any).Educacao;
+    }, error => {
+      console.error(error);
+    });
+
+    this.http.get('http://localhost:4242/api/project').subscribe(res => {
+      this.projects = (res as any).Projects;
+    }, error => {
+      console.error(error);
+    });
   }
 }
